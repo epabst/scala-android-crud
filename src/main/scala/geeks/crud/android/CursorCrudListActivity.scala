a28c35578bd9bcaf5c6898adb0aa494745929d1b
Eric Pabst <epabst@gmail.com>
2011-02-14 07:07:33 -0700

    Made populating the Views using the ListAdapater be more reliable.  (resolved todo).

--- src/main/scala/geeks/crud/android/CursorCrudListActivity.scala
+++ src/main/scala/geeks/crud/android/CursorCrudListActivity.scala
@@ -20,18 +20,17 @@ trait CursorCrudListActivity extends CrudListActivity[Cursor,Cursor,ContentValue
   lazy val dataSource: CursorAdapter = {
     val cursor = persistence.findAll
     val adapter = new SimpleCursorAdapter(this, rowLayout, cursor,
-      //provide the field names but making sure that they have the same length as the viewResourceIds.
-      //These aren't actually used by the ViewBinder below.
+      //provide the field names but making sure that they have the same length as the viewResourceIds
+      //since the SimpleCursorAdapter gets the field Views and the easiest way to recognize them is by the
+      //corresponding column name
       (cursor.getColumnNames.toList.tail ::: cursor.getColumnNames.toList.tail).slice(0, viewResourceIds.size).toArray,
       viewResourceIds.toArray)
+    val fieldByPersistedFieldName: Map[String,Field[Cursor,ContentValues]] = Map() ++ fields.flatMap(f => f.queryFieldNames.map((_,f)))
     adapter.setViewBinder(new SimpleCursorAdapter.ViewBinder() {
       def setViewValue(view: View, cursor: Cursor, columnIndex: Int) = {
-        //todo fix this to really correspond!!!
-        fields.foreach(field => if (field.queryFieldNames.contains(cursor.getColumnName(columnIndex))) {
-          field match {
-            case viewField: ViewField[Cursor,ContentValues] => viewField.readIntoFieldView(cursor, view)
-            case _ =>
-          }
+        fieldByPersistedFieldName.get(cursor.getColumnName(columnIndex)).map(_ match {
+          case viewField: ViewField[Cursor,ContentValues] => viewField.readIntoFieldView(cursor, view)
+          case _ =>
         })
         true
       }
