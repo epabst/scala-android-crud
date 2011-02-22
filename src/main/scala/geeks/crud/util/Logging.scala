caf9db36302457691422f12c0dc3b14bae63f2b9
Eric Pabst <epabst@gmail.com>
2011-02-22 13:15:44 -0700

    Output logging to the Console as well.

--- src/main/scala/geeks/crud/util/Logging.scala
+++ src/main/scala/geeks/crud/util/Logging.scala
@@ -11,21 +11,25 @@ import android.util.Log
 
 trait Logging {
   protected def debug(f: => String) {
+    println("DEBUG " + f)
     try Log.d(this.getClass.getName, f)
     catch { case e: RuntimeException if (e.getMessage == "Stub!") => }
   }
 
   protected def info(f: => String) {
+    println("INFO  " + f)
     try Log.i(this.getClass.getName, f)
     catch { case e: RuntimeException if (e.getMessage == "Stub!") => }
   }
 
   protected def warn(f: => String) {
+    println("WARN  " + f)
     try Log.w(this.getClass.getName, f)
     catch { case e: RuntimeException if (e.getMessage == "Stub!") => }
   }
 
   protected def error(f: => String) {
+    println("ERROR " + f)
     try Log.e(this.getClass.getName, f)
     catch { case e: RuntimeException if (e.getMessage == "Stub!") => }
   }
