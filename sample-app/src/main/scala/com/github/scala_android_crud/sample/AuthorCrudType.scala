e9f4ea25e70cf5480f4f3c9f403ed1c47bb5a12c
Eric Pabst <epabst@gmail.com>
2011-04-28 12:45:23 -0600

    Made crud types more readable.

--- sample-app/src/main/scala/com/github/scala_android_crud/sample/AuthorCrudType.scala
+++ sample-app/src/main/scala/com/github/scala_android_crud/sample/AuthorCrudType.scala
@@ -11,31 +11,23 @@ import com.github.triangle._
  */
 
 object AuthorCrudType extends SQLiteCrudEntityType {
-
   def entityName = "Author"
 
   def fields = List(Field[String](persisted("name"), viewId(R.id.name, textView)))
 
-  def activityClass = classOf[AuthorActivity]
-
-  def listActivityClass = classOf[AuthorListActivity]
-
-  def entryLayout = R.layout.author_entry
-
-  def rowLayout = R.layout.author_row
-
   //Use the same layout for the header
   def headerLayout = R.layout.author_row
-
   def listLayout = R.layout.entity_list
-
   def displayLayout = None
+  def entryLayout = R.layout.author_entry
+  def rowLayout = R.layout.author_row
 
   def cancelItemString = res.R.string.cancel_item
-
   def editItemString = R.string.edit_author
-
   def addItemString = R.string.add_author
+
+  def activityClass = classOf[AuthorActivity]
+  def listActivityClass = classOf[AuthorListActivity]
 }
 
 class AuthorListActivity extends CrudListActivity(AuthorCrudType, SampleApplication)
