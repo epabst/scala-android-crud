1770d1500fc497b8b6f56c6a0cf716e2ca19fa20
Eric Pabst <epabst@gmail.com>
2011-08-12 00:40:13 -0600

    Reordered results to match recent changes.

--- scala-android-crud/src/test/scala/com/github/scala_android/crud/generate/CrudUIGeneratorSpec.scala
+++ scala-android-crud/src/test/scala/com/github/scala_android/crud/generate/CrudUIGeneratorSpec.scala
@@ -28,12 +28,12 @@ class CrudUIGeneratorSpec extends Spec with MustMatchers {
   describe("detectResourceIdClasses") {
     it("must be able to find all of the R.id instances") {
       CrudUIGenerator.detectResourceIdClasses(classOf[CrudUIGeneratorSpec]) must
-              be (Seq(classOf[android.R.id], classOf[com.github.scala_android.crud.res.R.id], classOf[R.id]))
+              be (Seq(classOf[R.id], classOf[android.R.id], classOf[com.github.scala_android.crud.res.R.id]))
     }
 
     it("must look in parent packages to find the application R.id instance") {
       CrudUIGenerator.detectResourceIdClasses(classOf[foo.A]) must
-              be (Seq(classOf[android.R.id], classOf[com.github.scala_android.crud.res.R.id], classOf[R.id]))
+              be (Seq(classOf[R.id], classOf[android.R.id], classOf[com.github.scala_android.crud.res.R.id]))
     }
   }
 }
