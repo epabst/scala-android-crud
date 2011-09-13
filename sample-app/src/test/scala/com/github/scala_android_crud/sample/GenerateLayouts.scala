5a4dc23c72f58048a856faeae3876b6f0a4e13fd
Eric Pabst <epabst@gmail.com>
2011-09-13 12:34:43 -0600

    Generate layout iterating over the application's entities.

---
+++ sample-app/src/test/scala/com/github/scala_android_crud/sample/GenerateLayouts.scala
@@ -0,0 +1,16 @@
+package com.github.scala_android_crud.sample
+
+import com.github.scala_android.crud.generate.CrudUIGenerator
+
+/**
+ * A layout generator for the application.
+ * @author Eric Pabst (epabst@gmail.com)
+ * Date: 9/6/11
+ * Time: 11:17 PM
+ */
+
+object GenerateLayouts {
+  def main(args: Array[String]) {
+    SampleApplication.allEntities.foreach(CrudUIGenerator.generateLayouts(_))
+  }
+}
