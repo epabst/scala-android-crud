d9576acc29ced121e3d939f2156a059c319fb537
Eric Pabst <epabst@gmail.com>
2011-09-13 20:46:47 -0600

    Use Mockito instead of EasyMock.

--- scala-android-crud/src/test/scala/com/github/scala_android/crud/view/ViewFieldSpec.scala
+++ scala-android-crud/src/test/scala/com/github/scala_android/crud/view/ViewFieldSpec.scala
@@ -6,12 +6,13 @@ import com.github.triangle.PortableField._
 import com.github.scala_android.crud.persistence.CursorField._
 import ViewField._
 import android.view.View
-import org.scalatest.mock.EasyMockSugar
 import android.widget.{Spinner, LinearLayout, TextView}
 import com.xtremelabs.robolectric.RobolectricTestRunner
 import org.junit.Test
 import android.content.{Intent, Context}
 import com.github.scala_android.crud.action.Action.toUri
+import org.scalatest.mock.MockitoSugar
+import org.mockito.Mockito._
 
 
 /**
@@ -22,7 +23,7 @@ import com.github.scala_android.crud.action.Action.toUri
  */
 
 @RunWith(classOf[RobolectricTestRunner])
-class ViewFieldSpec extends MustMatchers with EasyMockSugar {
+class ViewFieldSpec extends MustMatchers with MockitoSugar {
   class MyEntity(var string: String, var number: Int)
 
   @Test
@@ -42,75 +43,62 @@ class ViewFieldSpec extends MustMatchers with EasyMockSugar {
     val view1 = mock[TextView]
     val view2 = mock[TextView]
     val view3 = mock[TextView]
-    expecting {
-      call(viewGroup.findViewById(101)).andReturn(view1).anyTimes
-      call(view1.setText(""))
-      call(viewGroup.findViewById(102)).andReturn(view2).anyTimes
-      call(view2.setText("Please Fill"))
-      call(viewGroup.findViewById(103)).andReturn(view3).anyTimes
-      call(view3.setText(""))
-    }
-    whenExecuting(viewGroup, view1, view2, view3) {
-      val stringField =
-        viewId(101, textView) +
-        viewId(102, fieldDirect[TextView,String](v => Option(v.getText.toString), v => v.setText(_), _.setText("Please Fill")))
-      stringField.setValue(viewGroup, None)
+    stub(viewGroup.findViewById(101)).toReturn(view1)
+    stub(viewGroup.findViewById(102)).toReturn(view2)
+    stub(viewGroup.findViewById(103)).toReturn(view3)
+    val stringField =
+      viewId(101, textView) +
+      viewId(102, fieldDirect[TextView,String](v => Option(v.getText.toString), v => v.setText(_), _.setText("Please Fill")))
+    stringField.setValue(viewGroup, None)
+    verify(view1).setText("")
+    verify(view2).setText("Please Fill")
 
-      val intField = viewId(103, intView)
-      intField.setValue(viewGroup, None)
-    }
+    val intField = viewId(103, intView)
+    intField.setValue(viewGroup, None)
+    verify(view3).setText("")
   }
 
   @Test
   def itMustOnlyCopyToAndFromViewByIdIfTheRightType() {
-    val context = mock[Context]
     val group = mock[View]
     val view = mock[TextView]
-    expecting {
-      call(group.findViewById(56)).andReturn(view).anyTimes
-    }
-    whenExecuting(context, group, view) {
-      val stringField = fieldDirect[MyEntity,String](e => e.string, e => e.string = _) +
-        viewId(56, fieldDirect[Spinner,String](
-          _ => throw new IllegalStateException("must not be called"),
-          _ => throw new IllegalStateException("must not be called")))
-      val myEntity1 = new MyEntity("my1", 1)
-      stringField.copy(myEntity1, group) //does nothing
-      stringField.copy(group, myEntity1) //does nothing
-    }
+    stub(group.findViewById(56)).toReturn(view)
+    val stringField = fieldDirect[MyEntity,String](e => e.string, e => e.string = _) +
+      viewId(56, fieldDirect[Spinner,String](
+        _ => throw new IllegalStateException("must not be called"),
+        _ => throw new IllegalStateException("must not be called")))
+    val myEntity1 = new MyEntity("my1", 1)
+    stringField.copy(myEntity1, group) //does nothing
+    stringField.copy(group, myEntity1) //does nothing
   }
 
   @Test
   def itMustOnlyCopyToAndFromViewByIdIfIdIsFound() {
     val context = mock[Context]
-    whenExecuting(context) {
-      val stringField = fieldDirect[MyEntity,String](e => e.string, e => e.string = _) +
-        viewId(56, fieldDirect[Spinner,String](
-          _ => throw new IllegalStateException("must not be called"),
-          _ => throw new IllegalStateException("must not be called")))
-      val myEntity1 = new MyEntity("my1", 1)
-      val group = new LinearLayout(context)
-      val view = new Spinner(context)
-      view.setId(100)
-      group.addView(view)
-      stringField.copy(myEntity1, group) //does nothing
-      stringField.copy(group, myEntity1) //does nothing
-    }
+    val stringField = fieldDirect[MyEntity,String](e => e.string, e => e.string = _) +
+      viewId(56, fieldDirect[Spinner,String](
+        _ => throw new IllegalStateException("must not be called"),
+        _ => throw new IllegalStateException("must not be called")))
+    val myEntity1 = new MyEntity("my1", 1)
+    val group = new LinearLayout(context)
+    val view = new Spinner(context)
+    view.setId(100)
+    group.addView(view)
+    stringField.copy(myEntity1, group) //does nothing
+    stringField.copy(group, myEntity1) //does nothing
   }
 
   @Test
   def itMustHandleUnparseableValues() {
     val context = mock[Context]
-    whenExecuting(context) {
-      val intField = intView + fieldDirect[MyEntity,Int](e => e.number, e => e.number = _)
-      val view = new TextView(context)
-      view.setText("twenty")
-      intField.getter(view) must be (None)
+    val intField = intView + fieldDirect[MyEntity,Int](e => e.number, e => e.number = _)
+    val view = new TextView(context)
+    view.setText("twenty")
+    intField.getter(view) must be (None)
 
-      val entity = new MyEntity("my1", 30)
-      intField.copy(view, entity)
-      entity.number must be (30)
-    }
+    val entity = new MyEntity("my1", 30)
+    intField.copy(view, entity)
+    entity.number must be (30)
   }
 
   @Test
@@ -135,28 +123,24 @@ class ViewFieldSpec extends MustMatchers with EasyMockSugar {
   @Test
   def itMustConvertNullToNone() {
     val context = mock[Context]
-    whenExecuting(context) {
-      val field = textView
-      val view = new TextView(context)
-      view.setText(null)
-      field.getter(view) must be (None)
+    val field = textView
+    val view = new TextView(context)
+    view.setText(null)
+    field.getter(view) must be (None)
 
-      view.setText("")
-      field.getter(view) must be (None)
-    }
+    view.setText("")
+    field.getter(view) must be (None)
   }
 
   @Test
   def itMustTrimStrings() {
     val context = mock[Context]
-    whenExecuting(context) {
-      val field = textView
-      val view = new TextView(context)
-      view.setText("  ")
-      field.getter(view) must be (None)
+    val field = textView
+    val view = new TextView(context)
+    view.setText("  ")
+    field.getter(view) must be (None)
 
-      view.setText(" hello world ")
-      field.getter(view) must be (Some("hello world"))
-    }
+    view.setText(" hello world ")
+    field.getter(view) must be (Some("hello world"))
   }
 }
