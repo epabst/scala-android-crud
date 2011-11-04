4b82bfac91353493cb83c5d2ffc9a8bac83585d4
Eric Pabst <epabst@gmail.com>
2011-11-03 21:10:57 -0600

    Made CrudPersistence.find* set IdPk.id if the result contains it.

--- scala-android-crud/src/main/scala/com/github/scala/android/crud/CrudPersistence.scala
+++ scala-android-crud/src/main/scala/com/github/scala/android/crud/CrudPersistence.scala
@@ -2,6 +2,8 @@ package com.github.scala.android.crud
 
 import action.UriPath
 import persistence.{IdPk, ListBufferEntityPersistence, SeqEntityPersistence, EntityPersistence}
+import com.github.triangle.PortableField._
+import com.github.triangle.FieldList
 
 /**
  * An EntityPersistence for a CrudType.
@@ -17,11 +19,14 @@ trait CrudPersistence extends EntityPersistence {
 
   def toUri(id: ID) = entityType.toUri(id)
 
+  private lazy val idPkField = entityType.IdField + field[IdPk, ID](_.id, e => e.id = _)
+  private lazy val fieldsIncludingIdPk = FieldList((idPkField +: entityType.fields): _*)
+
   def find[T <: AnyRef](uri: UriPath, instantiateItem: => T): Option[T] =
-    find(uri).map(entityType.transform(instantiateItem, _))
+    find(uri).map(fieldsIncludingIdPk.transform(instantiateItem, _))
 
   def findAll[T <: AnyRef](uri: UriPath, instantiateItem: => T): Seq[T] =
-    findAll(uri).map(entityType.transform(instantiateItem, _))
+    findAll(uri).map(fieldsIncludingIdPk.transform(instantiateItem, _))
 
   /** Saves the entity.  This assumes that the entityType's fields support copying from the given modelEntity. */
   def save(modelEntity: IdPk): ID = save(modelEntity.id, entityType.transform(entityType.newWritable, modelEntity))
---
+++ scala-android-crud/src/test/scala/com/github/scala/android/crud/CrudPersistenceSpec.scala
@@ -0,0 +1,39 @@
+package com.github.scala.android.crud
+
+import common.PlatformTypes
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.matchers.MustMatchers
+import org.scalatest.Spec
+import com.github.scala.android.crud.action.UriPath
+import persistence.IdPk
+
+/**
+ * A behavior specification for [[com.github.scala.android.crud.persistence.EntityPersistence]].
+ * @author Eric Pabst (epabst@gmail.com)
+ * Date: 11/1/11
+ * Time: 9:32 PM
+ */
+@RunWith(classOf[JUnitRunner])
+class CrudPersistenceSpec extends Spec with MustMatchers with MyEntityTesting {
+  class MyEntity(givenId: Option[PlatformTypes#ID] = None) extends IdPk {
+    this.id = givenId
+  }
+  val persistence = new SeqCrudPersistence[MyEntity] {
+    def entityType = new MyEntityType(this, null)
+    def crudContext = null
+    def findAll(uri: UriPath) = Seq(new MyEntity(entityType.UriPathId.getter(uri)))
+  }
+
+  it("find must set IdPk.id") {
+    val uri = persistence.entityType.toUri(100L)
+    val Some(result) = persistence.find(uri, new MyEntity)
+    result.id must be (Some(100L))
+  }
+
+  it("findAll must set IdPk.id") {
+    val uri = persistence.entityType.toUri(100L)
+    val result = persistence.findAll(uri, new MyEntity).head
+    result.id must be (Some(100L))
+  }
+}
