00f091b871daca08588c4f4e23f672212b9640dd
Eric Pabst <epabst@gmail.com>
2011-12-28 15:10:44 -0700

    Use ScalaTest for UriPathSpec.

--- scala-android-crud/src/test/scala/com/github/scala/android/crud/common/UriPathSpec.scala
+++ scala-android-crud/src/test/scala/com/github/scala/android/crud/common/UriPathSpec.scala
@@ -1,55 +1,53 @@
 package com.github.scala.android.crud.common
 
 import org.junit.runner.RunWith
-import org.junit.Test
 import com.github.scala.android.crud.MyCrudType
 import org.scalatest.matchers.MustMatchers
-import com.xtremelabs.robolectric.RobolectricTestRunner
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.Spec
 
 /**
- * A behavior specification for {@link UriPath}.
+ * A behavior specification for [[com.github.scala.android.crud.common.UriPath]].
  * @author Eric Pabst (epabst@gmail.com)
  * Date: 9/22/11
  * Time: 10:40 PM
  */
-@RunWith(classOf[RobolectricTestRunner])
-class UriPathSpec extends MustMatchers {
+@RunWith(classOf[JUnitRunner])
+class UriPathSpec extends Spec with MustMatchers {
   val entityName = MyCrudType.entityName
 
-  @Test
-  def mustFindTheIdFollowingTheEntityName() {
-    UriPath("foo").findId(entityName) must be (None)
-    UriPath(entityName).findId(entityName) must be (None)
-    UriPath(entityName, "123").findId(entityName) must be (Some(123))
-    UriPath(entityName, "123", "foo").findId(entityName) must be (Some(123))
-    UriPath(entityName, "blah").findId(entityName) must be (None)
+  describe("findId") {
+    it("must find the id following the entity name") {
+      UriPath("foo").findId(entityName) must be (None)
+      UriPath(entityName).findId(entityName) must be (None)
+      UriPath(entityName, "123").findId(entityName) must be (Some(123))
+      UriPath(entityName, "123", "foo").findId(entityName) must be (Some(123))
+      UriPath(entityName, "blah").findId(entityName) must be (None)
+    }
   }
 
-  @Test
-  def upToIdOfMustStripOfWhateverIsAfterTheID() {
-    val uri = UriPath("abc", "123", entityName, "456", "def")
-    uri.upToIdOf(entityName) must be (UriPath("abc", "123", entityName, "456"))
+  describe("upToIdOf") {
+    it("must strip of whatever is after the ID") {
+      val uri = UriPath("abc", "123", entityName, "456", "def")
+      uri.upToIdOf(entityName) must be (UriPath("abc", "123", entityName, "456"))
+    }
+
+    it("must not fail if no ID found but also preserve what is there already") {
+      val uri = UriPath("abc", "123", "def")
+      uri.upToIdOf(entityName).segments.startsWith(uri.segments) must be (true)
+    }
   }
 
-  @Test
-  def upToIdOfMustNotFailIfNoIDFoundButAlsoPreserveWhatIsThereAlready() {
-    val uri = UriPath("abc", "123", "def")
-    uri.upToIdOf(entityName).segments.startsWith(uri.segments) must be (true)
-  }
-
-  @Test
-  def mustConvertFromString() {
+  it("must convert from a string") {
     val uriPath = UriPath("abc", "123", "def")
     UriPath(uriPath.toString) must be (uriPath)
   }
 
-  @Test
-  def mustConvertFromEmptyUri() {
+  it("must convert from an empty uri") {
     UriPath(UriPath.EMPTY.toString) must be(UriPath.EMPTY)
   }
 
-  @Test
-  def mustConvertFromEmptyString() {
+  it ("must convert from an empty string") {
     UriPath("") must be (UriPath.EMPTY)
   }
 }
