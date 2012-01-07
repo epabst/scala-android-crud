4776877a5adac952a13e080971712a8b3f2037f4
Eric Pabst <epabst@gmail.com>
2012-01-07 15:14:03 -0700

    Simplified methods provided by EntityFieldInfo.

--- scala-android-crud/src/main/scala/com/github/scala/android/crud/generate/EntityFieldInfo.scala
+++ scala-android-crud/src/main/scala/com/github/scala/android/crud/generate/EntityFieldInfo.scala
@@ -1,33 +1,30 @@
 package com.github.scala.android.crud.generate
 
-import android.view.View
 import com.github.scala.android.crud.view.AndroidResourceAnalyzer._
-import com.github.triangle.{PortableField, FieldList, SubjectField, BaseField}
+import com.github.triangle.{PortableField, FieldList, BaseField}
 import com.github.scala.android.crud.view.{FieldLayout, ViewField, ViewIdNameField, ViewIdField}
 import com.github.scala.android.crud.persistence.{EntityType, CursorField}
 
 case class EntityFieldInfo(field: BaseField, rIdClasses: Seq[Class[_]]) {
   private lazy val updateablePersistedFields = CursorField.updateablePersistedFields(field, rIdClasses)
 
-  private lazy val viewIdFields: List[ViewIdField[_]] = field.deepCollect[ViewIdField[_]] {
-    case matchingField: ViewIdField[_] => matchingField
-  }
-  private lazy val viewIdNameFields: List[ViewIdNameField[_]] = field.deepCollect[ViewIdNameField[_]] {
-    case matchingField: ViewIdNameField[_] => matchingField
+  private def viewFields(field: BaseField): List[ViewField[_]] = field.deepCollect {
+    case matchingField: ViewField[_] => matchingField
   }
 
-  private[generate] def fieldsWithViewSubject(field: BaseField): List[SubjectField] = field.deepCollect[SubjectField] {
-    case matchingField: SubjectField if classOf[View].isAssignableFrom(matchingField.subjectManifest.erasure) => {
-      matchingField
+  lazy val viewIdFieldInfos: List[ViewIdFieldInfo] = {
+    val viewIdFields: List[ViewIdField[_]] = field.deepCollect[ViewIdField[_]] {
+      case matchingField: ViewIdField[_] => matchingField
     }
-  }
+    val viewIdNameFields: List[ViewIdNameField[_]] = field.deepCollect[ViewIdNameField[_]] {
+      case matchingField: ViewIdNameField[_] => matchingField
+    }
 
-  private lazy val viewFieldsWithId = this.fieldsWithViewSubject(FieldList.toFieldList(viewIdFields))
-  lazy val otherViewFields = this.fieldsWithViewSubject(field).filterNot(viewFieldsWithId.contains)
-  lazy val viewIdFieldInfos: List[ViewIdFieldInfo] = viewIdNameFields.map(f => ViewIdFieldInfo(f.viewResourceIdName, f)) ++
-    viewIdFields.map { viewIdField =>
-      ViewIdFieldInfo(resourceFieldWithIntValue(rIdClasses, viewIdField.viewResourceId).getName, viewIdField)
-    }
+    viewIdNameFields.map(f => ViewIdFieldInfo(f.viewResourceIdName, f)) ++
+      viewIdFields.map { viewIdField =>
+        ViewIdFieldInfo(resourceFieldWithIntValue(rIdClasses, viewIdField.viewResourceId).getName, viewIdField)
+      }
+  }
 
   lazy val isDisplayable: Boolean = !viewIdFieldInfos.isEmpty
   def isPersisted: Boolean = !updateablePersistedFields.isEmpty
@@ -35,6 +32,11 @@ case class EntityFieldInfo(field: BaseField, rIdClasses: Seq[Class[_]]) {
 
   lazy val displayableViewIdFieldInfos: List[ViewIdFieldInfo] = if (isDisplayable) viewIdFieldInfos else Nil
   lazy val updateableViewIdFieldInfos: List[ViewIdFieldInfo] = if (isUpdateable) viewIdFieldInfos else Nil
+
+  lazy val otherViewFields = {
+    val viewFieldsWithinViewIdFields = viewFields(FieldList(viewIdFieldInfos.map(_.field):_*))
+    viewFields(field).filterNot(viewFieldsWithinViewIdFields.contains)
+  }
 }
 
 case class ViewIdFieldInfo(id: String, displayName: String, field: PortableField[_]) {
